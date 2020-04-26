2387ff61619bb07892f8881ac886dc082b88c121
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-04-26 17:11:09 +0100

    behavior subject

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/User.model';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  private userSubject:BehaviorSubject<User>;
+  public userOb:Observable<User>;
+
+  constructor() { 
+    let user=new User('Marwen','Hanzouli',25995310,'marwenhanzouli@gmail.com','1234');
+    user.role="USER";
+    this.userSubject=new BehaviorSubject(user);
+    this.userOb=this.userSubject.asObservable();
+  }
 
   register(user:User){
     
