6d9ccd471ce041f7108f2d70ede8e7af1030b441
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-04-26 03:27:07 +0100

    update register

--- src/app/home/register/register.component.ts
+++ src/app/home/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from 'src/app/helpers/validators';
 
 @Component({
   selector: 'app-register',
@@ -7,8 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  submitted: boolean=false;
+  loading:boolean=false;
+  type:string;
+  eye:string;
+  color:string;
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.initForm();
+    this.type="password";
+    this.eye="eye-off";
+    this.color="primary";
+  }
+  initForm()
+  {
+    this.registerForm = this.formBuilder.group(
+      {
+        firstName:['', Validators.required],
+        lastName:['', Validators.required],
+        phone:['', [Validators.required,Validators.pattern(/^[0-9]*$/)]],
+        email: ['', [Validators.required,Validators.email]],
+        password: ['', Validators.required],
+        repassword: ['', Validators.required]
+      },{
+        validator: MustMatch('password', 'repassword')
+      }
+    );
+  }
+  get f() { return this.registerForm.controls; }
+  changePassword(){
+    if(this.type==="password"){
+      this.type="text";
+      this.eye="eye";
+    }else{
+      this.type="password";
+      this.eye="eye-off";
+      this.color="primary"
+    }
+  }
+  register(){
+    this.submitted=true;
+    if(this.registerForm.invalid)
+    {
+      return;
+    }
+    
+    console.log(this.registerForm.value)
+  }
 
 }
