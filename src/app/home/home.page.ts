9e0e30616fe94536ce0b124a190ea220de6225e2
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-04-26 01:32:34 +0100

    update in home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  
+  private login:boolean;
+  private register:boolean;
 
   constructor() {}
+
+  segmentChanged(event){
+    if(event.detail.value==="signup")
+    {
+      this.login=false;
+      this.register=true;
+    }else if(event.detail.value==="signin"){
+      this.login=true;
+      this.register=false;
+    }
+  }
+
+  ngOnInit(): void {
+    this.login=true;
+    this.register=false;
+  }
 
 }
