0e19886c4570a33e3629a308e6fe131ea1cd0992
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-04-27 23:04:26 +0100

    updates

--- src/app/dashboard/ask/ask.component.ts
+++ src/app/dashboard/ask/ask.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from 'src/app/services/photo.service';
+import { ActionSheetController, ModalController } from '@ionic/angular';
+import { PhotoComponent } from '../photo/photo.component';
 
 @Component({
   selector: 'app-ask',
@@ -7,8 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AskComponent implements OnInit {
 
-  constructor() { }
+  constructor(public photoService: PhotoService,
+              public actionSheetController: ActionSheetController,
+              public modalController: ModalController) { }
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.photoService.loadSaved();
+  }
+  public async showActionSheet(photo, position) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Display',
+        icon: 'eye',
+        handler: () => {
+          this.presentModal(position);
+        }
+      }, {
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, 
+      {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
+  async presentModal(postion:number) {
+    const modal = await this.modalController.create({
+      component: PhotoComponent,
+      cssClass:'color-icons',
+      componentProps: {
+        'photo': this.photoService.getPhoto(postion)
+      }
+    });
+    return await modal.present();
+  }
 }
