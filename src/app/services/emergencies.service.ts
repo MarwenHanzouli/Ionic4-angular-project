fac4b14d772195c9e82e990b103557b86584ac8e
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-04-30 02:12:28 +0100

    add emergency first version

--- src/app/services/emergencies.service.ts
+++ src/app/services/emergencies.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { AngularFireDatabase, AngularFireList,AngularFireObject } from 'angularfire2/database';
+import { Emergency } from '../models/Emergency.model';
+import {  Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmergenciesService {
 
-  constructor() { }
+  private apiUrl=environment.apiUrl;
+  public emergenciesList: AngularFireList<Emergency>;
+
+  private emergencies:Observable<Emergency[]>;
+  constructor(private db: AngularFireDatabase,
+              private firestore:AngularFirestore) {
+    this.getEmergencies();
+    this.emergencies.subscribe(console.log)
+    firestore.collection('emergencies').valueChanges().subscribe(console.log);
+  }
+  addEmergency(em:Emergency){
+    em.affected=false;
+    this.emergenciesList.push(em);
+    this.firestore.collection('emergencies').add({
+      lat:em.lat,
+      lng:em.lng,
+      description:em.description,
+      affected:em.affected,
+      ambilancier:em.ambilancier
+    });
+  }
+
+  getEmergencies() {
+    this.emergenciesList=this.db.list("/emergencies");
+    this.emergencies=this.emergenciesList.valueChanges();
+  }
 }
